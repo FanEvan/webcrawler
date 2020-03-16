8b27c728f5bbb2affee40607b5cb405c1e5d3e78
FanEvan <60687733+FanEvan@users.noreply.github.com>
2020-03-16 23:00:22 +0800

    Update webscrawler.py

--- webscrawler.py
+++ webscrawler.py
@@ -1,39 +1,27 @@
-from pyquery import PyQuery as pq
-import requests
-response = requests.get("https://movies.yahoo.com.tw/movie_thisweek.html")
-doc = pq(response.text)
-movie_names = doc(".release_info .release_movie_name>.gabtn").text()  
-movie_levels = doc(".levelbox").text()   
-movie_namesList = movie_names.split()    
-movie_namesList1 = []
-for j in movie_namesList:
-    if j == '電影版':
-        continue
-    else:
-        movie_namesList1.append(j)
+from selenium import webdriver
 
-movie_expectScore = movie_levels.split()[1::5]  
-movie_expectPoint = []
-for m in movie_expectScore:
-    mm = m.replace("%","")
-    int(mm)
-    movie_expectPoint.append(mm)
-    
-movie_timetables = []  
-for movie_timetable in doc(".release_btn.color_btnbox .btn_s_time.gabtn").items("a"):
-    movie_timetables.append(movie_timetable.attr("href"))
-    
-movie_expectList = {}  
-for x in range(len(movie_namesList1)):
-    movie_expectList[movie_namesList1[x]] = movie_expectPoint[x]
-    
-movie_timetableList = {}  
-for z in range(len(movie_namesList1)):
-    movie_timetableList[movie_namesList1[z]] = movie_timetables[z]
-    
-n = 1  
-for y in movie_expectList.keys():
-    y1 = int(movie_expectList[y])
-    if y1 > 80:
-        print("待看電影清單",n,":",y,movie_timetableList[y])
-        n += 1
+driver_path = "/Users/kuoyufan/pytest/webdriver/chromedriver"
+option = webdriver.ChromeOptions()
+option.add_argument("headless") 
+driver = webdriver.Chrome(driver_path)
+driver.implicitly_wait(10)
+
+url = "https://movies.yahoo.com.tw/movie_thisweek.html"
+name_path = "//main//div/div[@class='release_movie_name']/a"
+score_path = "//*[@id='content_l']//dl/dt/div[2]/span"
+
+driver.get(url)
+movie_names = driver.find_elements_by_xpath(name_path)
+expect_scores = driver.find_elements_by_xpath(score_path)
+
+namelists, scorelists, linklists = [],[],[]
+for name, score, link in zip(movie_names, expect_scores, movie_names):
+    namelists.append(name.text)
+    scorelists.append(int((str(score.text).replace("%",""))))
+    linklists.append(name.get_attribute("href"))
+
+mylist={}
+for i in range(len(scorelists)):
+    if scorelists[i] >= 80:
+        mylist[namelists[i]] = linklists[i]
+print("必看電影清單: ", mylist)
